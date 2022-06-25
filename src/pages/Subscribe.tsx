b59ea8a410119619bcd4ca34915a83f89a4e1668
K4ME <guilhermegodencio@gmail.com>
2022-06-25 11:26:46 -0300

    lesson 5 - add codegen and finished the project

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,15 +1,7 @@
-import { gql, useMutation } from "@apollo/client";
 import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
-
-const CREATE_SUBSCRIBER_MUTATION = gql`
-  mutation CreateSubscriber ($name: String!, $email: String!) {
-    createSubscriber(data: {name: $name, email: $email}) {
-      id
-    }
-  }
-`
+import { useCreateSubscriberMutation } from "../graphql/generated";
 
 export function Subscribe() {
   const navigate = useNavigate();
@@ -17,14 +9,14 @@ export function Subscribe() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
-  const [createSubscriber, { loading }] = useMutation(CREATE_SUBSCRIBER_MUTATION);
+  const [createSubscriber, { loading }] = useCreateSubscriberMutation()
 
-  async function handleSubscribe(event: FormEvent){
+  async function handleSubscribe(event: FormEvent) {
     event.preventDefault();
 
     createSubscriber({
       variables: {
-        name, 
+        name,
         email,
       }
     })
@@ -34,12 +26,12 @@ export function Subscribe() {
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div  className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
         <div className="max-w-[640px]">
-          <Logo/>
+          <Logo />
 
           <h1 className="mt-8 text-[2.5rem] leading-tight">
-            Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React </strong> 
+            Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React </strong>
           </h1>
 
           <p className="mt-4 text-gray-200 leading-relaxed">
@@ -48,24 +40,24 @@ export function Subscribe() {
         </div>
 
         <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-          <strong  className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+          <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
           <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
-            <input 
+            <input
               className="bg-gray-900 rounded px-5 py-4 h-014"
               type="text"
               placeholder="Seu nome completo"
               onChange={event => setName(event.target.value)}
             />
 
-            <input 
+            <input
               className="bg-gray-900 rounded px-5 py-4 h-014"
               type="email"
               placeholder="Digite seu e-mail"
               onChange={event => setEmail(event.target.value)}
             />
 
-            <button 
+            <button
               type="submit"
               disabled={loading}
               className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50">
